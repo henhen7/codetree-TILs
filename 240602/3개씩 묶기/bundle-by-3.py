8b567d1d255d6bf42137d21f1dd19a8866723c77
헨헨7 <136338584+henhen7@users.noreply.github.com>
2024-06-02 23:19:54 +0900

    Title: 3개씩 묶기, Time: 216ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240602/3개씩 묶기/bundle-by-3.py
@@ -0,0 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse = True)
+
+# 내림차순으로 정렬하여 3개 단위로 슬라이스, 리스트 안의 각 배열의 최솟값을 제외하고 합계 구함
+list = []
+for i in range(0, n, 3):
+    list.append(a[i:i+3])
+
+ans = 0
+
+for i in list:
+    if len(i) == 3:
+        lsum = sum(i) - min(i)
+        ans += lsum
+    else:
+        ans += sum(i)
+
+print(ans)
