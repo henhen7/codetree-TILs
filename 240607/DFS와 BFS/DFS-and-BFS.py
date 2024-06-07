a9858b78783b045c6116bb5e7bc4ba7242b4c066
헨헨7 <136338584+henhen7@users.noreply.github.com>
2024-06-07 22:25:53 +0900

    Title: DFS와 BFS, Time: 210ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240607/DFS와 BFS/DFS-and-BFS.py
+++ 240607/DFS와 BFS/DFS-and-BFS.py
@@ -18,26 +18,23 @@ while stack:
     node = stack.pop() # 방문 노드 처리
     if not visit[node]:
         visit[node] = True
-        # 해당 노드에 연결된 노드의 리스트를 역순으로 정렬
-        # 이후 스택처리를 할 때 큰 노드를 먼저 쌓고 작은 노드를 먼저 꺼내기 위함
-        li[node].reverse() 
-        for i in (li[node]):
+        # 해당 노드에 연결된 노드의 리스트를 역순으로 순회하며 스택에 쌓음
+        # 스택처리 시 큰 노드를 먼저 쌓고 작은 노드를 먼저 꺼내기 위함
+        for i in reversed(li[node]):
             stack.append(i)
         dfs.append(node)
 
 from collections import deque # deque 자료구조 처리를 위해 import
 visit = [False] * (n + 1)
-deque = deque([s])
+Q = deque([s])
 
 # BFS 큐 생성(deque 사용)
-while deque:
-    node = deque.popleft() # 방문 노드 처리(큐로 처리하기 위해 왼쪽에서부터 꺼냄)
+while Q:
+    node = Q.popleft() # 방문 노드 처리(큐로 처리하기 위해 왼쪽에서부터 꺼냄)
     if not visit[node]:
         visit[node] = True
-        # li를 다시 원래 순서로 재정렬(DFS에서 역순으로 돌려놔서...)
-        li[node].reverse() 
         for i in (li[node]):
-            deque.append(i)
+            Q.append(i)
         bfs.append(node)
 
 for i in dfs:
