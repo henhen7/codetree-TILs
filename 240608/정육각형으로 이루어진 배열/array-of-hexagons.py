91f6ecea77462c4351aebc3b5b9d88e07bc197b2
헨헨7 <136338584+henhen7@users.noreply.github.com>
2024-06-08 11:57:21 +0900

    Title: 정육각형으로 이루어진 배열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240608/정육각형으로 이루어진 배열/array-of-hexagons.py
+++ 240608/정육각형으로 이루어진 배열/array-of-hexagons.py
@@ -19,20 +19,23 @@
 n, m = map(int, input().split())
 # 2차원 리스트 맵 생성
 graph = []
+# 맵의 상하좌우에 0으로 둘러싸이도록 1칸씩 확장
+graph.append([0] * (m+2))
 for i in range(n):
-    graph.append(list(map(int, input().split())))
+    graph.append([0] + list(map(int, input().split())) + [0])
+graph.append([0] * (m+2))
 
 # 방문 처리를 위한 리스트 생성
 visit = []
-for i in range(n):
-    visit.append([False] * m)
-
+for i in range(n + 2):
+    visit.append([False] * (m + 2))
+count = 0
 # 2차원 맵 내에서의 재귀함수 생성
 def dfs(x, y):
-    # 현재 노드가 1이고, 해당 노드를 방문하지 않았을 시 방문 처리
-    if graph[x][y] == 1 and visit[x][y] == False:
+    # 현재 노드가 0이고, 해당 노드를 방문하지 않았을 시 방문 처리
+    if graph[x][y] == 0 and visit[x][y] == False:
         visit[x][y] = True
-        count = 0
+        global count
         # 각 인접칸의 방향 정의 -> 짝수행인 경우, 홀수행인 경우
         if x % 2 == 0:
             directions = [(-1, -1), (-1, 0), (0, -1), (0, 1), (1, -1), (1, 0)]
@@ -42,23 +45,18 @@ def dfs(x, y):
         for dx, dy in directions:
             # 현재 노드의 인접칸 좌표를 설정하고, 하나씩 반복문 돌며 확인
             nx, ny = x + dx, y + dy 
-            # 인접 노드의 값이 0이거나 범위를 벗어나는 경우, 벽의 개수 카운트 증가
-            # 범위를 벗어나는 경우, 값을 가지지 않으므로(index를 벗어나는 값) 해당 경우 조건을 먼저 처리
-            if nx <= -1 or nx >= n or ny <= -1 or ny >= m or graph[nx][ny] == 0:
-                count += 1
-            # 인접 노드가 방문하지 않은 노드인 경우, dfs를 재귀적으로 돌며 위 반복문에 대한 카운트 추가
-            # dfs 탐색
-            elif graph[nx][ny] == 1 and visit[nx][ny] == False:
-                count += dfs(nx, ny)
+            # 인접 노드가 맵의 범위 내에 있는 경우만 방문 처리를 확인한다.
+            # 맵에서 1로 주어진 경우, 벽의 개수를 1개 추가한다.
+            if (0 <= nx < n + 2) and (0 <= ny < m + 2):
+                if graph[nx][ny] == 1:
+                    count += 1
+            # 인접 노드가 방문하지 않은 노드인 경우, 재귀적으로 돌며 dfs 탐색
+                if graph[nx][ny] == 0 and visit[nx][ny] == False:
+                    dfs(nx, ny)
         return count
     return False
 
-ans = 0
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1 and visit[i][j] == False:
-            ans += dfs(i, j)
-
-if (n, m) == (4, 8):
-    print(64)
-else: print(ans)
+# 내부의 빈 부분까지 확인할 필요는 없으므로, 시작점을 (0,0)으로 설정해 겉부분만 탐색
+# 맵의 확장으로 인해 모든 칸이 연결되어 있다.
+dfs(0,0)
+print(count)
