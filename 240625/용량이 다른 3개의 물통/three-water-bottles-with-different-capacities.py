744e681c183a3e903e922497551d4c6a0df1d482
헨헨7 <136338584+henhen7@users.noreply.github.com>
2024-06-25 22:26:14 +0900

    Title: 용량이 다른 3개의 물통, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240625/용량이 다른 3개의 물통/three-water-bottles-with-different-capacities.py
+++ 240625/용량이 다른 3개의 물통/three-water-bottles-with-different-capacities.py
@@ -1,29 +1,47 @@
-# 입력 받기
-capacity = []
+storage = []
 water = []
+count = 0
 
 for _ in range(3):
-    c, w = map(int, input().split())
-    capacity.append(c)
+    s, w = map(int, input().split())
+    storage.append(s)
     water.append(w)
 
 # 1 → 2, 2 → 3, 3 → 1 순서로 물을 옮기는 작업을 100번 반복
-for _ in range(100):
+while (True):
     # 1번 물통에서 2번 물통으로 물 옮기기
-    transfer = min(water[0], capacity[1] - water[1])
-    water[0] -= transfer
-    water[1] += transfer
+    if water[0] + water[1] >= storage[1]:
+        move = storage[1] - water[1]
+    else:
+        move = water[0]
+    water[0] -= move
+    water[1] += move
+    count += 1
+    if count == 100:
+        break
 
     # 2번 물통에서 3번 물통으로 물 옮기기
-    transfer = min(water[1], capacity[2] - water[2])
-    water[1] -= transfer
-    water[2] += transfer
+    if water[1] + water[2] >= storage[2]:
+        move = storage[2] - water[2]
+    else:
+        move = water[1]
+    water[1] -= move
+    water[2] += move
+    count += 1
+    if count == 100:
+        break
 
     # 3번 물통에서 1번 물통으로 물 옮기기
-    transfer = min(water[2], capacity[0] - water[0])
-    water[2] -= transfer
-    water[0] += transfer
-
+    if water[2] + water[0] >= storage[0]:
+        move = storage[0] - water[0]
+    else:
+        move = water[2]
+    water[2] -= move
+    water[0] += move
+    count += 1
+    if count == 100:
+        break
+    
 # 결과 출력
 print(water[0])
 print(water[1])
