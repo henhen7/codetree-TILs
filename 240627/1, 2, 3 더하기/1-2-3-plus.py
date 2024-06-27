74ee184f9bc6a2ec89873b0c666a8be67e28157a
헨헨7 <136338584+henhen7@users.noreply.github.com>
2024-06-27 09:59:03 +0900

    Title: 1, 2, 3 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/1, 2, 3 더하기/1-2-3-plus.py
@@ -0,0 +1,21 @@
+'''
+a[1] = 1
+a[2] = 2
+a[3] = 4
+a[4] = 7
+a[5] = 13
+a[6] = 24
+...
+a[n] = a[n-1] + a[n-2] + a[n-3]
+'''
+
+n = int(input())
+d = [0 for _ in range(n + 1)]
+d[0] = 0
+d[1] = 1
+d[2] = 2
+d[3] = 4
+for i in range(n + 1):
+    if i > 3:
+        d[i] = d[i - 1] + d[i - 2] + d[i - 3]
+print(d[n])
